3066d30416c233701234c7a64895576b6b3c34d1
Daniel Robinson <daniel.robinson.software@gmail.com>
2013-10-25 02:02:25 +0100

    Accepting input from the console. Basic validation. Lacking tests for stdin and stdout.

--- src/calculator.js
+++ src/calculator.js
@@ -3,9 +3,37 @@
   var calculator = {};
 
   calculator.acceptCharacter = function(input){
-    return '1';
+    if (input == '=') return true;
+    return parseFloat(input) == input;
+  };
+
+  calculator.setup = function(){
+    var stdin = process.stdin;
+    stdin.setRawMode( true );
+    stdin.resume();
+    stdin.setEncoding( 'utf8' );
+    return stdin;
+  };
+
+  calculator.run = function(stdin){
+    stdin.on( 'data', function(key){
+      if (calculator.acceptCharacter(key)){        
+        process.stdout.write( key );
+        if ( key === '=' ) {
+          process.exit(1);
+        }
+      }     
+    });
+  };
+
+  calculator.start = function(){
+    var stdin = calculator.setup();
+    calculator.run(stdin);
   };
 
   module.exports = calculator;
+  exports.start = calculator.start;
+
+  calculator.start();
 
 })();
--- test/numeric-entry-tests.js
+++ test/numeric-entry-tests.js
@@ -1,11 +1,23 @@
 var expect = require("expect.js");
 var calculator = require("../src/calculator.js");
+var events = require("events");
 
 describe('calculator', function(){
 
     it('should allow a number to be the first character entered', function(){
         var accepted = calculator.acceptCharacter('1');
-        expect(accepted).to.be('1');
+        expect(accepted).to.be(true);
     });
     
+    it('should not allow a letter to be the first character entered', function(){
+        var accepted = calculator.acceptCharacter('a');
+        expect(accepted).to.be(false);
+    });
+   
+    it('should allow the equals sign to be the first character', function(){
+        var accepted = calculator.acceptCharacter('=');
+        expect(accepted).to.be(true);
+    });
+   
+
 });
